3093fbac2c597f1aa15fd23ce32963163e75820c
geekSiddharth <siddharth16268@iiitd.ac.in>
2017-06-18 23:02:02 +0530

    PlugIn For Level Set Algo in ImageJ added

---
+++ src/java/PlugInForLevelSet.java
@@ -0,0 +1,198 @@
+import ij.IJ;
+import ij.ImagePlus;
+import ij.ImageStack;
+import ij.gui.Roi;
+import ij.measure.Measurements;
+import ij.measure.ResultsTable;
+import ij.plugin.Duplicator;
+import ij.plugin.ImageCalculator;
+import ij.plugin.filter.ParticleAnalyzer;
+import ij.plugin.filter.PlugInFilter;
+import ij.plugin.frame.RoiManager;
+import ij.process.ImageProcessor;
+import levelsets.algorithm.ActiveContours;
+import levelsets.algorithm.LevelSetImplementation;
+import levelsets.ij.ImageContainer;
+import levelsets.ij.ImageProgressContainer;
+import levelsets.ij.StateContainer;
+import trainableSegmentation.WekaSegmentation;
+
+public class PlugInForLevelSet implements PlugInFilter {
+
+    @Override
+    public int setup(String s, ImagePlus imagePlus) {
+
+        // TODO: Check what kind of images can be given. Make it Robust
+        return DOES_ALL;
+    }
+
+    @Override
+    public void run(ImageProcessor imageProcessor) {
+
+        //ImagePlus object of the original image. Weka Segmentation is performed on it.
+        ImagePlus imagePlusOrignal = new ImagePlus("Original Input Image", imageProcessor);
+        //copy is a duplicate of the original image. It is used in later part of the program to refine edges using LevelSet
+        ImagePlus copy = new Duplicator().run(imagePlusOrignal);
+
+        ImagePlus finalAnswer = new ImagePlus();
+
+        //stores the classifies Image
+        ImagePlus imagePlus = getClassifiedImage(imagePlusOrignal);
+        IJ.log("Image Classified");
+
+
+        imagePlus = smooth(imagePlus);
+        imagePlus = threshold(imagePlus);
+
+
+        //Declare ROI Manager for Particle Analyzer
+        RoiManager rm = new RoiManager();
+        ParticleAnalyzer.setRoiManager(rm);
+
+
+        ParticleAnalyzer pa = getParticleAnalyzer();
+        pa.analyze(imagePlus);
+
+        boolean firstTime = true;
+        for (int i = 0; i < rm.getCount(); i++) {
+
+            rm.select(i);
+            copy.setRoi(rm.getRoi(i));
+
+            ImagePlus seg = getSegmentedImage(copy);
+
+            if (firstTime) {
+                firstTime = false;
+                finalAnswer = new Duplicator().run(seg);
+            } else {
+                ImageCalculator imageCalculator = new ImageCalculator();
+                finalAnswer = imageCalculator.run("min create", finalAnswer, seg);
+
+            }
+        }
+        finalAnswer.show();
+
+        //TODO: Run a Level set algo to find edge of the whole embryo. Then use it after thresholding[To remove stuff outside the embryo] and after getting results[To prevent overshooting of levetset]
+    }
+
+    protected ImagePlus getClassifiedImage(ImagePlus imagePlus) {
+        //Trainable Weka Segmentation
+        WekaSegmentation wekaSegmentator = new WekaSegmentation(imagePlus);
+        //Classifier Model file Loaded. TODO: Decide the path
+        wekaSegmentator.loadClassifier("F:\\FijiDev\\TryGlobal\\src\\fast.model");
+        //true means probability map will be given
+        wekaSegmentator.applyClassifier(true);
+        ImagePlus classifiedImage = wekaSegmentator.getClassifiedImage();
+        classifiedImage.show();
+
+
+        //TODO: Figure out a better way to do the below. This DIRTY CODE
+        //IJ.run(classifiedImage, "Next Slice [>]", "");
+        IJ.run(classifiedImage, "Delete Slice", "");
+
+        IJ.saveAsTiff(classifiedImage, "ProbMap");
+        IJ.run(classifiedImage, "8-bit", "");
+        return classifiedImage;
+    }
+
+    private ImagePlus smooth(ImagePlus imagePlus) {
+        //smoothing TODO: Improve Smoothing functions
+        IJ.run(imagePlus, "Smooth", "");
+        IJ.run(imagePlus, "Smooth", "");
+        IJ.run(imagePlus, "Smooth", "");
+
+        return imagePlus;
+    }
+
+    private ImagePlus threshold(ImagePlus imagePlus) {
+        //Thresholding TODO: Automatically decide the lower limit
+        IJ.setThreshold(imagePlus, 175.0, 255.0, "Black & White"); //We chose a lower limit such that we get significant portion of inner cell
+        IJ.run(imagePlus, "Convert to Mask", "");
+        return imagePlus;
+    }
+
+    private ImagePlus morph(ImagePlus imagePlus) {
+        //Morphological Operations TODO: Improve
+        IJ.run(imagePlus, "Close-", "");
+        IJ.run(imagePlus, "Close-", "");
+        IJ.run(imagePlus, "Erode", "");
+        IJ.run(imagePlus, "Erode", "");
+        IJ.run(imagePlus, "Close-", "");
+        IJ.run(imagePlus, "Close-", "");
+        IJ.run(imagePlus, "Dilate", "");
+        IJ.run(imagePlus, "Dilate", "");
+        IJ.run(imagePlus, "Dilate", "");
+
+        return imagePlus;
+    }
+
+    private ParticleAnalyzer getParticleAnalyzer() {
+        //Parameters for ParticleAnalyzer TODO: Research about them
+        int opts = ParticleAnalyzer.ADD_TO_MANAGER;
+        int meas = Measurements.ALL_STATS;
+        double minSize = Math.PI * Math.pow((10.0 / 2), 2.0);
+        double maxSize = Math.PI * Math.pow((150.0 / 2), 2.0);
+        ResultsTable rt1 = new ResultsTable();
+
+        ParticleAnalyzer pa = new ParticleAnalyzer(opts, meas, rt1, minSize, maxSize);
+        return pa;
+    }
+
+    //originalImage must have an roi selected in it
+    private ImagePlus getSegmentedImage(ImagePlus originalImage) {
+
+
+        //roi for LS
+        Roi roi = originalImage.getRoi();
+
+        //creating ImageContainer
+        ImageContainer ic = new ImageContainer(originalImage);
+
+//        Creating ImageProgressContainer
+//        If we don't want the progress report of level set algorithm
+        ImageProgressContainer progressImage = null;
+
+//        For progress report uncomment the code below
+//        ImageProgressContainer progressImage = new ImageProgressContainer();
+//        progressImage.duplicateImages(ic);
+//        progressImage.createImagePlus("Segmentation progress of " + copy.getTitle());
+//        progressImage.showProgressStep();
+
+
+        // Create a initial state map out of the roi
+        StateContainer sc_roi = new StateContainer();
+        System.out.println("current slice: " + originalImage.getCurrentSlice());
+        sc_roi.setROI(roi, ic.getWidth(), ic.getHeight(), ic.getImageCount(), originalImage.getCurrentSlice());
+
+        //For which side to evolve
+        sc_roi.setExpansionToInside(false);
+
+        double convergence = 0.0050;
+        double advection = 1.0;
+        double curvature = 1.0;
+        double grey_tol = 1.0;
+
+        LevelSetImplementation ls = new ActiveContours(ic, progressImage, sc_roi, convergence, advection, curvature, grey_tol);
+        for (int iter = 0; iter < 25; iter++) {
+            if (!ls.step(100)) {
+                break;
+            }
+        }
+        StateContainer sc_final = ls.getStateContainer();
+
+        // Convert sc_final into binary image ImageContainer and display
+        if (sc_final == null) {
+            IJ.log("Wow man. sc_final is empty. You lost. Now die.");
+        }
+        ImageStack stack = new ImageStack(originalImage.getWidth(), originalImage.getHeight());
+        for (ImageProcessor bp : sc_final.getIPMask()) {
+            stack.addSlice(null, bp);
+        }
+        ImagePlus seg = originalImage.createImagePlus();
+        seg.setStack("Segmentation of " + originalImage.getTitle(), stack);
+        seg.setSlice(originalImage.getCurrentSlice());
+
+        return seg;
+    }
+
+}
